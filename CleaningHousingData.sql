4845bdf92809c59c46ce13c7f826758c17a01853
Pat Canale <73977982+patrickmcanale@users.noreply.github.com>
2021-08-17 15:42:16 -0500

    Add files via upload
    Initial Commit

---
+++ CleaningHousingData.sql
@@ -0,0 +1,127 @@
+SELECT *
+FROM Project.dbo.NashvilleHousing
+
+
+--Change Date-standard date format
+
+
+SELECT SaleDateConverted, CONVERT(Date,SaleDate)
+FROM Project.dbo.NashvilleHousing
+
+UPDATE Project.dbo.NashvilleHousing
+SET SaleDate - CONVERT(Date,SaleDate)
+
+ALTER TABLE Project.dbo.NashvilleHousing
+SET SaleDateConverted - CONVERT(Date,SaleDate)
+
+
+--Populate Property Address
+
+
+SELECT *
+FROM Project.dbo.NashvilleHousing
+order by parcelid
+
+SELECT a.parcelid, a.propertyAddress, b.ParcelID, b.propertyAddress, ISNULL(a.propertyAddress,b.propertyAddress)
+FROM Project.dbo.NashvilleHousing as a
+JOIN Project.dbo.NashvilleHousing as b
+	ON a.parcelid=b.parcelid
+	AND a.[UniqueID ]<>b.[UniqueID ]
+WHERE a.propertyAddress is null
+
+UPDATE a
+SET propertyAddress = ISNULL(a.propertyAddress,b.propertyAddress)
+FROM Project.dbo.NashvilleHousing as a
+JOIN Project.dbo.NashvilleHousing as b
+	ON a.parcelid=b.parcelid
+	AND a.[UniqueID ]<>b.[UniqueID ]
+WHERE a.propertyAddress is null
+
+
+--break out address into individual columns (Address, city, state)
+
+SELECT 
+PARSENAME(REPLACE(OwnerAddress,',','.'),3),
+PARSENAME(REPLACE(OwnerAddress,',','.'),2),
+PARSENAME(REPLACE(OwnerAddress,',','.'),1)
+FROM Project.dbo.NashvilleHousing
+
+ALTER TABLE Project.dbo.NashvilleHousing
+Add OwnerSplitAddress Nvarchar(255)
+
+UPDATE Project.dbo.NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress,',','.'),3)
+
+ALTER TABLE Project.dbo.NashvilleHousing
+Add OwnerSplitCity Nvarchar(255)
+
+UPDATE Project.dbo.NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress,',','.'),2)
+
+ALTER TABLE Project.dbo.NashvilleHousing
+Add OwnerSplitState Nvarchar(255)
+
+UPDATE Project.dbo.NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress,',','.'),1)
+
+
+--"sold as vacand" field- change 'y/n' to 'yes/no'
+
+
+SELECT DISTINCT(soldasvacant), COUNT(soldasvacant)
+from Project.dbo.NashvilleHousing
+group by soldasvacant
+order by 2
+
+
+SELECT soldasvacant,
+	CASE WHEN soldasvacant = 'Y' THEN 'Yes'
+		 WHEN soldasvacant = 'N' THEN 'No'
+		 ELSE soldasvacant
+		 END
+FROM Project.dbo.NashvilleHousing
+
+
+UPDATE Project.dbo.NashvilleHousing
+SET soldasvacant = CASE WHEN soldasvacant = 'Y' THEN 'Yes'
+		 WHEN soldasvacant = 'N' THEN 'No'
+		 ELSE soldasvacant
+		 END
+
+
+--remove duplicates
+
+WITH RowNumCTE AS(
+SELECT *,
+	ROW_NUMBER() OVER(
+	PARTITION BY ParcelID,
+				PropertyAddress,
+				SalePrice,
+				SaleDate,
+				LegalReference
+				ORDER BY
+					UniqueID
+					)row_num
+FROM Project.dbo.NashvilleHousing
+)
+DELETE
+from RowNumCTE
+where row_rum > 1
+--order by PropertyAddress
+select*
+from RowNumCTE
+where row_rum > 1
+order by PropertyAddress
+
+
+--Delete Unused Columns
+
+
+SELECT * 
+FROM Project.dbo.NashvilleHousing
+
+ALTER TABLE Project.dbo.NashvilleHousing
+DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress
+
+ALTER TABLE Project.dbo.NashvilleHousing
+DROP COLUMN SaleDate
